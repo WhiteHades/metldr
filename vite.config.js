b1eab8d0dbf2f4a1ced21d722429b8055fbe416d
WhiteHades <44260523+WhiteHades@users.noreply.github.com>
2025-10-19 19:57:06 +0200

    still init stage

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,18 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      input: {
+        side_panel: 'index.html',
+        background: 'src/background/index.js'
+      },
+      output: {
+        entryFileNames: '[name].js',
+        chunkFileNames: 'chunks/[name].js',
+        assetFileNames: 'assets/[name][extreme]',
+      }
+    }
+  }
 })
