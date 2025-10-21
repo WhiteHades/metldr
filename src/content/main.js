5d037665946fc2335d6f91d34a83175e0da934b1
WhiteHades <44260523+WhiteHades@users.noreply.github.com>
2025-10-22 01:58:56 +0200

    fix(content): handle context invalidation

--- src/content/main.js
+++ src/content/main.js
@@ -3,7 +3,21 @@ console.log("metldr content script loaded")
 document.addEventListener('mouseup', () => {
   const selectedText = window.getSelection().toString().trim();
   if (selectedText.length > 0) {
-    console.log("selected text:", selectedText)
-    chrome.runtime.sendMessage({ type: 'SELECTION_TEXT', text: selectedText })
+    try {
+      if (!chrome.runtime?.id) {
+        console.log('extension context lost, skipping message');
+        return;
+      }
+      
+      console.log("selected text:", selectedText)
+      chrome.runtime.sendMessage({ type: 'SELECTION_TEXT', text: selectedText })
+    }
+    catch (e) {
+      if (e.message.includes('Extension context invalidated')) {
+        console.log('extension was reloaded, please refresh this page');
+      } else {
+        console.error(e);
+      }
+    }
   }
 })
