52021d13920e465f98709c556279b9b68553467f
WhiteHades <44260523+WhiteHades@users.noreply.github.com>
2025-12-17 19:45:24 +0600

    refactor: migrate entrypoints and config to typescript

--- src/background/index.ts
+++ src/background/index.ts
@@ -1,6 +1,6 @@
 import '@inboxsdk/core/background';
 
-import { BackgroundBootstrap } from '../services/BackgroundBootstrap.js';
+import { BackgroundBootstrap } from '../services/BackgroundBootstrap';
 
 BackgroundBootstrap.init().catch(err => {
   console.error('metldr: failed to initialize background service:', err);
---
+++ src/config/secrets.ts
@@ -0,0 +1,1 @@
+export const INBOXSDK_APP_ID = 'sdk_metldr_86b31d5ad6';
